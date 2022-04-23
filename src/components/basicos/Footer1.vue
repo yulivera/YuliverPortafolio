81205a35735cafea709a2358f28b1b3c6d4d9e52
yulivera <yuliverjimenez@gmail.com>
2022-04-23 13:28:23 -0400

    edit footer

--- src/components/basicos/Footer1.vue
+++ src/components/basicos/Footer1.vue
@@ -1,70 +1,7 @@
 <template>
 	<footer class="ftco-footer">
 	    <div class="container ">	    	
-	        <div class="row mb-3">
-	          <div class="col-md">	          	
-	            <div class="ftco-footer-widget mb-3 ml-md-4">            	
-	              <h2 class="ftco-heading-2 widget-title">Sitios Oficiales<span></span></h2>
-	              <ul class="list-unstyled photo">
-	                <li>
-	                	<a href="https://www.java.com/es/" class="img" 
-	                	style="background-image: url(/images/java.png);">
-	                	</a>
-	                </li>
-	                <li><a href="https://www.php.net/" class="img" 
-	                	style="background-image: url(/images/php.png);"></a></li>
-	                <li><a href="https://www.mysql.com/" class="img" 
-	                	style="background-image: url(/images/mysql.jpg);"></a></li>
-	                
-	                <li><a href="https://vuejs.org/" class="img" 
-	                	style="background-image: url(/images/vuejs.jpg);"></a></li>
-	                
-	                <li><a href="https://www.php.net/" class="img" 
-	                	style="background-image: url(/images/php.png);"></a></li>
-	                <li><a href="https://getbootstrap.com/" class="img" 
-	                	style="background-image: url(/images/boots.jpg);"></a></li>
-	              </ul>
-	            </div>
-	          </div>
-
-	          <div class="col-md">
-	             <div class="ftco-footer-widget mb-4">
-	              <h2 class="ftco-heading-2 widget-title">Links <span></span></h2>
-	              <ul class="list-unstyled categories">
-	              	<li><a href="/">
-	              		<font-awesome-icon icon="location-arrow" />
-	              		Inicio</a></li>
-	              	<li><a href="/portafolio">
-						<font-awesome-icon icon="location-arrow" />
-	              		Portafolio</a></li>
-	                <li><a href="/sobreMi">
-	                <font-awesome-icon icon="location-arrow" />
-	            	Sobre Mi </a></li>	                
-	              </ul>
-	            </div>
-	          </div>
-
-	          <div class="col-md">
-	            <div class="ftco-footer-widget mb-4">
-	            	<h2 class="ftco-heading-2 widget-title">Contactame <span></span></h2>
-	            	<div class="block-23 mb-4">
-		              <ul>
-		                <li>
-		                	<span class="icon"><font-awesome-icon icon="map-marker" /></span>
-		                	<span class="text">Barquisimeto - Venezuela</span></li>
-		                <li>		             
-		                	<span class="icon" >
-		                	<font-awesome-icon icon="phone" /></span>
-		                	<span class="text">+00 ---------</span></li>
-
-		                <li>
-		                	<span class="icon" ><font-awesome-icon icon="envelope" /></span>
-		                	<span class="text">yuliverjimenez@gmail.com</span></li>
-		              </ul>
-		            </div>
-	            </div>
-	          </div>
-	        </div>
+	        
 	        <div class="row">
 	          <div class="col-md-12 text-center">
 				<hr>
