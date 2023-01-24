b774f7d84215603fa5403863ee690a4b2c167210
Yuliver <yuliverjimenez@gmail.com>
2023-01-24 16:35:40 +0000

    experi

--- src/views/SobreMi.vue
+++ src/views/SobreMi.vue
@@ -2,21 +2,18 @@
   <div class="SectionMi">
     <Presentacion></Presentacion>
     <Educacion></Educacion>
-    <Experiencia></Experiencia>
   </div>
 </template>
 
 <script>
 import Presentacion from "@/components/sobremi/Presentacion.vue";
 import Educacion from "@/components/sobremi/Educacion.vue";
-import Experiencia from "@/components/sobremi/Experiencia.vue";
 
 export default {
   name: "SobreMi",
   components: {
     Presentacion,
-    Educacion,
-    Experiencia,
+    Educacion
   },
   data() {
     return {};
