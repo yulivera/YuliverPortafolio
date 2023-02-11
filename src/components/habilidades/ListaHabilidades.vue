e08c4dcbe213ea46bca1ff338086fbd638812b50
Yuliver <yuliverjimenez@gmail.com>
2023-02-11 05:37:35 +0000

    habilidades

--- src/components/habilidades/ListaHabilidades.vue
+++ src/components/habilidades/ListaHabilidades.vue
@@ -5,18 +5,8 @@
         <div v-for="(list, index) in habilidades" :key="index" class="col-md-6">
           <div class="progress-wrap">
             <h3>{{ list.nombre }}</h3>
-            <div class="progress">
-              <div
-                class="progress-bar"
-                role="progressbar"
-                aria-valuenow="90"
-                aria-valuemin="0"
-                aria-valuemax="100"
-                :style="{ width: list.porcentaje + '%' }"
-              >
-                <span>{{ list.porcentaje }}%</span>
-              </div>
-            </div>
+            
+
           </div>
         </div>
       </div>
